ff0cbe6c295db652e61439e1e0ea9a54bd0f30be
hal58th <hal58th@12k>
2024-05-25 17:56:35 -0700

    Improving the script

--- ScenarioEditor/scripts/proptrack.sh
+++ ScenarioEditor/scripts/proptrack.sh
@@ -1,47 +1,62 @@
 #!/bin/bash
 
 # Notes
-# 45 Degrees
+# 30 Degrees
   # rotation = 0,0,0.25,1
-# 90 Degrees
+# -30 Degrees (I think)
+  # rotation = 0,0,-0.25,1
+# 45 Degrees
   # rotation = 0,0,0.25,0.5
 # 0 Degrees (flat)
   # rotation = 0,0,0,0.5
+# Rotated 90 Z but flat
+  # rotation = 0,0.7,0,0.7
+# Rotated 180 Z but flat
+  # rotation = 0,0,0,1
+# Rotated 270 Z but flat
+  # rotation = 0,-0.7,0,0.7
+# Rotaated Straight up
+  # rotation = 0.5,0.5,0.5,0.5
+# Rotated straight up (opposite)
+  # rotation = -0.5,0.5,-0.5,0.5
 
-rows=500
-columns=6
+propName="Platforms.Ramps.RampFlat2x1"
 
-# It looks like positions are in y,z,x or x,z,y position. God knows why?
+rows=20
+columns=1
 
-ypos=-111
-zpos=16.2554466247559
-xpos=248
+xpos=244
+ypos=21.6004810333252
+zpos=-24.3616638183594
 
-xpos_offset="+ 0"
-ypos_offset="+ 4"
+xpos_offset="- 2"
+ypos_offset="+ 0"
 zpos_offset="+ 0"
 
+rotation="0.5,0.5,0.5,0.5"
+
+# Do not update these
 second_xpos=$xpos
 second_ypos=$ypos
 second_zpos=$zpos
 
-second_xpos_offset="+ 2"
+second_xpos_offset="+ 0"
 second_ypos_offset="+ 0"
-second_zpos_offset="+ 0"
+second_zpos_offset="- 2"
 
 for i in $( seq 1 $columns )
 do
   for i in $( seq 1 $rows )
   do
     guid=$(cat /proc/sys/kernel/random/uuid)
-    echo "	Scenarios.Data.ScnProp
+echo "	Scenarios.Data.ScnProp
 	{
-		propName = Platforms.Ramps.RampFlat2x1
+		propName = $propName
 		visibleInStage = -1
 		persistAfterMissionEnd = True
 		scale = 1,1,1
-		absPos = $xpos,$zpos,$ypos
-		rotation = 0,0.5,0,0.5
+		absPos = $xpos,$ypos,$zpos
+		rotation = $rotation
 		guid = $guid
 	}"
     xpos=`echo $xpos $xpos_offset | bc`
